4a4ca6369c380cb183a6c0b14bcb5f9dbfaee030
kanat <taalayuulu@inbox.ru>
2016-04-21 19:31:00 +0600

    create new post controller

---
+++ src/Application/Sonata/AdminBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kanat
+ * Date: 4/21/16
+ * Time: 7:30 PM
+ */
+namespace Application\Sonata\AdminBundle;
+
+class AppExtension
+{
+
+}
---
+++ src/Application/Sonata/AdminBundle/PostAdmin.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kanat
+ * Date: 4/21/16
+ * Time: 7:24 PM
+ */
+
+namespace Application\Sonata\AdminBundle;
+
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+
+class PostAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array(
+                'label' => 'Post Title'
+            ))
+            ->add('author', 'entity', array(
+                'class' => 'AppBundle\Entity\User'
+            ))
+
+            // if no type is specified, SonataAdminBundle tries to guess it
+            ->add('body')
+
+            // ...
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('author')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('slug')
+            ->add('author')
+        ;
+    }
+
+    // Fields to be shown on show action
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('title')
+            ->add('slug')
+            ->add('author')
+        ;
+    }
+}
