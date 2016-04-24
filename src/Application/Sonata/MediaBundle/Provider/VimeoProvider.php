3cc1df2e065025f67650ee6bc5fb497416c09d0e
kanat <taalayuulu@inbox.ru>
2016-04-24 19:40:33 +0600

    add new translator for media bundle

---
+++ src/Application/Sonata/MediaBundle/Provider/VimeoProvider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kanat
+ * Date: 4/22/16
+ * Time: 8:07 PM
+ */
+
+namespace Application\Sonata\MediaBundle\Provider;
+
+use Sonata\MediaBundle\Provider\BaseProvider;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\MediaBundle\Model\MediaInterface;
+use Symfony\Component\Form\Form;
+
+class VimeoProvider extends BaseProvider
+{
+}
